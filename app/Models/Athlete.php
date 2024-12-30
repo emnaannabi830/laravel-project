25109a862a8ca5b5fe6738df22d1cc100bb2e86b
Ayman-ing <aymanfeki1@gmail.com>
2024-12-30 23:44:39 +0100

    fix the project

--- app/Models/Athlete.php
+++ app/Models/Athlete.php
@@ -1,6 +1,14 @@
 <?php
+// app/Models/Athlete.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
 class Athlete extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'age', 'coach_id'];
 
     public function coach()
--- app/Models/Coach.php
+++ app/Models/Coach.php
@@ -1,10 +1,14 @@
 <?php
+// app/Models/Coach.php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coach extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'email'];
 
     public function athletes()
